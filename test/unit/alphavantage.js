d44e019172d32ada3566134eabd3476bf75f5fb6
Stefan Gründel <stefan.gruendel@gmail.com>
2021-01-05 18:22:52 +0100

    Fix unit tests.

--- test/unit/alphavantage.js
+++ test/unit/alphavantage.js
@@ -13,114 +13,120 @@ describe('alphavantage', () => {
     });
 
     describe('#queryDailyAdjusted()', () => {
-        it('should work für AMZN since 2020-12-01', async() => {
-            const results = await alphavantage.queryDailyAdjusted('AMZN', '2020-12-01');
+        it('should work für AMZN since 2020-12-31', async() => {
+            const results = await alphavantage.queryDailyAdjusted('AMZN', '2020-12-31');
             expect(results.length).greaterThan(1);
-            expect(results[0].symbol).to.equal('AMZN');
-            expect(results[0].date).to.equal('2020-12-31');
-            expect(results[0].open).to.equal(3275);
-            expect(results[0].high).to.equal(3282.9219);
-            expect(results[0].low).to.equal(3241.2);
-            expect(results[0].close).to.equal(3256.93);
-            expect(results[0].adjustedClose).to.equal(3256.93);
-            expect(results[0].volume).to.equal(2957206);
-            expect(results[0].dividendAmount).to.equal(0);
-            expect(results[0].splitCoefficient).to.equal(1);
+            const result = results.slice(-1)[0];
+            expect(result.symbol).to.equal('AMZN');
+            expect(result.date).to.equal('2020-12-31');
+            expect(result.open).to.equal(3275);
+            expect(result.high).to.equal(3282.9219);
+            expect(result.low).to.equal(3241.2);
+            expect(result.close).to.equal(3256.93);
+            expect(result.adjustedClose).to.equal(3256.93);
+            expect(result.volume).to.equal(2957206);
+            expect(result.dividendAmount).to.equal(0);
+            expect(result.splitCoefficient).to.equal(1);
         });
     });
 
     describe('#queryDailyAdjusted()', () => {
         it('should work für AMZN since <future date>', async() => {
-            const results = await alphavantage.queryDailyAdjusted('AMZN', '9999-12-01');
+            const results = await alphavantage.queryDailyAdjusted('AMZN', '9999-12-31');
             expect(results.length).equal(0);
         });
     });
 
     describe('#querySMA()', () => {
-        it('should work für AMZN since 2020-12-01', async() => {
-            const results = await alphavantage.querySMA('AMZN', 38, '2020-12-01');
+        it('should work für AMZN since 2020-12-31', async() => {
+            const results = await alphavantage.querySMA('AMZN', 38, '2020-12-31');
             expect(results.length).greaterThan(1);
-            expect(results[0].symbol).to.equal('AMZN');
-            expect(results[0].date).to.equal('2020-12-31');
-            expect(results[0].sma).to.equal(3172.9108);
+            const result = results.slice(-1)[0];
+            expect(result.symbol).to.equal('AMZN');
+            expect(result.date).to.equal('2020-12-31');
+            expect(result.sma).to.equal(3172.9108);
         });
     });
 
     describe('#querySMA()', () => {
         it('should work für AMZN since <future date>', async() => {
-            const results = await alphavantage.querySMA('AMZN', 38, '9999-12-01');
+            const results = await alphavantage.querySMA('AMZN', 38, '9999-12-31');
             expect(results.length).equal(0);
         });
     });
 
     describe('#queryEMA()', () => {
-        it('should work für AMZN since 2020-12-01', async() => {
-            const results = await alphavantage.queryEMA('AMZN', 50, '2020-12-01');
+        it('should work für AMZN since 2020-12-31', async() => {
+            const results = await alphavantage.queryEMA('AMZN', 50, '2020-12-31');
             expect(results.length).greaterThan(1);
-            expect(results[0].symbol).to.equal('AMZN');
-            expect(results[0].date).to.equal('2020-12-31');
-            expect(results[0].ema).to.equal(3186.5997);
+            const result = results.slice(-1)[0];
+            expect(result.symbol).to.equal('AMZN');
+            expect(result.date).to.equal('2020-12-31');
+            expect(result.ema).to.equal(3186.5997);
         });
     });
 
     describe('#queryEMA()', () => {
         it('should work für AMZN since <future date>', async() => {
-            const results = await alphavantage.queryEMA('AMZN', 50, '9999-12-01');
+            const results = await alphavantage.queryEMA('AMZN', 50, '9999-12-31');
             expect(results.length).equal(0);
         });
     });
 
     describe('#queryMACD()', () => {
-        it('should work für AMZN since 2020-12-01', async() => {
-            const results = await alphavantage.queryMACD('AMZN', '2020-12-01');
+        it('should work für AMZN since 2020-12-31', async() => {
+            const results = await alphavantage.queryMACD('AMZN', '2020-12-31');
             expect(results.length).greaterThan(1);
-            expect(results[0].symbol).to.equal('AMZN');
-            expect(results[0].date).to.equal('2020-12-31');
-            expect(results[0].macd).to.equal(28.6142);
-            expect(results[0].hist).to.equal(10.7848);
-            expect(results[0].signal).to.equal(17.8294);
+            const result = results.slice(-1)[0];
+            expect(result.symbol).to.equal('AMZN');
+            expect(result.date).to.equal('2020-12-31');
+            expect(result.macd).to.equal(28.6142);
+            expect(result.hist).to.equal(10.7848);
+            expect(result.signal).to.equal(17.8294);
         });
     });
 
     describe('#queryMACD()', () => {
         it('should work für AMZN since <future date>', async() => {
-            const results = await alphavantage.queryMACD('AMZN', '9999-12-01');
+            const results = await alphavantage.queryMACD('AMZN', '9999-12-31');
             expect(results.length).equal(0);
         });
     });
 
     describe('#queryRSI()', () => {
-        it('should work für AMZN since 2020-12-01', async() => {
-            const results = await alphavantage.queryRSI('AMZN', 14, '2020-12-01');
+        it('should work für AMZN since 2020-12-31', async() => {
+            const results = await alphavantage.queryRSI('AMZN', 14, '2020-12-31');
             expect(results.length).greaterThan(1);
-            expect(results[0].symbol).to.equal('AMZN');
-            expect(results[0].date).to.equal('2020-12-31');
-            expect(results[0].rsi).to.equal(56.0688);
+            const result = results.slice(-1)[0];
+            expect(result.symbol).to.equal('AMZN');
+            expect(result.date).to.equal('2020-12-31');
+            expect(result.rsi).to.equal(56.0688);
         });
     });
 
     describe('#queryRSI()', () => {
         it('should work für AMZN since <future date>', async() => {
-            const results = await alphavantage.queryRSI('AMZN', 14, '9999-12-01');
+            const results = await alphavantage.queryRSI('AMZN', 14, '9999-12-31');
             expect(results.length).equal(0);
         });
     });
 
     describe('#queryBBands()', () => {
-        it('should work für AMZN since 2020-12-01', async() => {
-            const results = await alphavantage.queryBBands('AMZN', 20, '2020-12-01');
+        it('should work für AMZN since 2020-12-31', async() => {
+            const results = await alphavantage.queryBBands('AMZN', 20, '2020-12-31');
             expect(results.length).greaterThan(1);
-            expect(results[0].symbol).to.equal('AMZN');
-            expect(results[0].date).to.equal('2020-12-31');
-            expect(results[0].lower).to.equal(3078.7905);
-            expect(results[0].upper).to.equal(3313.8985);
-            expect(results[0].middle).to.equal(3196.3445);
+            const result = results.slice(-1)[0];
+            expect(result.symbol).to.equal('AMZN');
+            expect(result.date).to.equal('2020-12-31');
+            expect(result.lower).to.equal(3078.7905);
+            expect(result.upper).to.equal(3313.8985);
+            expect(result.middle).to.equal(3196.3445);
         });
     });
 
     describe('#queryBBands()', () => {
         it('should work für AMZN since <future date>', async() => {
-            const results = await alphavantage.queryBBands('AMZN', 20, '9999-12-01');
+            const results = await alphavantage.queryBBands('AMZN', 20, '9999-12-31');
             expect(results.length).equal(0);
         });
     });
