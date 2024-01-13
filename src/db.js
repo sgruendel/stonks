f43627f2a0a9dffe26f3b6e56d3f05bf893df538
Stefan Gründel <stefan.gruendel@gmail.com>
2024-01-13 12:53:47 +0100

    fix tests for new dynamoose

--- src/db.js
+++ src/db.js
@@ -32,28 +32,30 @@ async function handleThroughput(callback, params, attempt = 1) {
             // exponential backoff with jitter,
             // see https://aws.amazon.com/de/blogs/architecture/exponential-backoff-and-jitter/
             const temp = Math.min(CAP, BACK_OFF * Math.pow(2, attempt));
-            const sleep = temp / 2 + Math.floor(Math.random() * temp / 2);
+            const sleep = temp / 2 + Math.floor((Math.random() * temp) / 2);
             logger.debug('DynamoDB: sleeping for ' + sleep + ' on attempt ' + attempt + ', temp ' + temp);
-            await new Promise(resolve => setTimeout(resolve, sleep));
+            await new Promise((resolve) => setTimeout(resolve, sleep));
             return handleThroughput(callback, params, ++attempt);
         } else throw e;
     }
 }
 
-var exports = module.exports = {};
+var exports = (module.exports = {});
 
-exports.handleThroughput = async function(callback, params) {
+exports.handleThroughput = async function (callback, params) {
     return handleThroughput(callback, params);
 };
 
-exports.CompanyOverview = dynamoose.model('CompanyOverview',
-    new dynamoose.Schema({
-        symbol: {
-            type: String,
-            validate: (symbol) => symbol.length > 0,
-            required: true,
-        },
-    }, {
+exports.CompanyOverviewSchema = {
+    symbol: {
+        type: String,
+        validate: (symbol) => symbol.length > 0,
+        required: true,
+    },
+};
+exports.CompanyOverview = dynamoose.model(
+    'CompanyOverview',
+    new dynamoose.Schema(exports.CompanyOverviewSchema, {
         saveUnknown: true,
         timestamps: true,
     }),
@@ -61,154 +63,161 @@ exports.CompanyOverview = dynamoose.model('CompanyOverview',
         create: CONFIG_CREATE,
         prefix: CONFIG_PREFIX,
         waitForActive: CONFIG_WAIT_FOR_ACTIVE,
-    });
+    },
+);
 
-exports.DailyAdjusted = dynamoose.model('DailyAdjusted',
-    new dynamoose.Schema({
-        symbol: {
-            type: String,
-            validate: (symbol) => symbol.length > 0,
-            required: true,
-        },
-        date: {
-            type: String,
-            validate: (date) => date.length > 0,
-            required: true,
-            rangeKey: true,
-        },
-        open: {
-            type: Number,
-            validate: (open) => open >= 0,
-            required: true,
-        },
-        high: {
-            type: Number,
-            validate: (high) => high >= 0,
-            required: true,
-        },
-        low: {
-            type: Number,
-            validate: (low) => low >= 0,
-            required: true,
-        },
-        close: {
-            type: Number,
-            validate: (close) => close >= 0,
-            required: true,
-        },
-        adjustedClose: {
-            type: Number,
-            validate: (adjustedClose) => adjustedClose >= 0,
-            required: true,
-        },
-        volume: {
-            type: Number,
-            validate: (volume) => volume >= 0,
-            required: true,
-        },
-        dividendAmount: {
-            type: Number,
-            validate: (dividendAmount) => dividendAmount >= 0,
-            required: true,
-        },
-        splitCoefficient: {
-            type: Number,
-            validate: (splitCoefficient) => splitCoefficient >= 0,
-            required: true,
-        },
-    }, {
+exports.DailyAdjustedSchema = {
+    symbol: {
+        type: String,
+        validate: (symbol) => symbol.length > 0,
+        required: true,
+    },
+    date: {
+        type: String,
+        validate: (date) => date.length > 0,
+        required: true,
+        rangeKey: true,
+    },
+    open: {
+        type: Number,
+        validate: (open) => open >= 0,
+        required: true,
+    },
+    high: {
+        type: Number,
+        validate: (high) => high >= 0,
+        required: true,
+    },
+    low: {
+        type: Number,
+        validate: (low) => low >= 0,
+        required: true,
+    },
+    close: {
+        type: Number,
+        validate: (close) => close >= 0,
+        required: true,
+    },
+    adjustedClose: {
+        type: Number,
+        validate: (adjustedClose) => adjustedClose >= 0,
+        required: true,
+    },
+    volume: {
+        type: Number,
+        validate: (volume) => volume >= 0,
+        required: true,
+    },
+    dividendAmount: {
+        type: Number,
+        validate: (dividendAmount) => dividendAmount >= 0,
+        required: true,
+    },
+    splitCoefficient: {
+        type: Number,
+        validate: (splitCoefficient) => splitCoefficient >= 0,
+        required: true,
+    },
+};
+exports.DailyAdjusted = dynamoose.model(
+    'DailyAdjusted',
+    new dynamoose.Schema(exports.DailyAdjustedSchema, {
         timestamps: true,
     }),
     {
         create: CONFIG_CREATE,
         prefix: CONFIG_PREFIX,
         waitForActive: CONFIG_WAIT_FOR_ACTIVE,
-    });
+    },
+);
 
-exports.TechnicalIndicators = dynamoose.model('TechnicalIndicators',
-    new dynamoose.Schema({
-        symbol: {
-            type: String,
-            validate: (symbol) => symbol.length > 0,
-            required: true,
-        },
-        date: {
-            type: String,
-            validate: (date) => date.length > 0,
-            required: true,
-            rangeKey: true,
-        },
-        sma15: {
-            type: Number,
-            validate: (sma) => sma >= 0,
-        },
-        sma20: {
-            type: Number,
-            validate: (sma) => sma >= 0,
-        },
-        sma50: {
-            type: Number,
-            validate: (sma) => sma >= 0,
-        },
-        sma100: {
-            type: Number,
-            validate: (sma) => sma >= 0,
-        },
-        sma200: {
-            type: Number,
-            validate: (sma) => sma >= 0,
-        },
-        ema12: {
-            type: Number,
-            validate: (ema) => ema >= 0,
-        },
-        ema20: {
-            type: Number,
-            validate: (ema) => ema >= 0,
-        },
-        ema26: {
-            type: Number,
-            validate: (ema) => ema >= 0,
-        },
-        ema50: {
-            type: Number,
-            validate: (ema) => ema >= 0,
-        },
-        ema100: {
-            type: Number,
-            validate: (ema) => ema >= 0,
-        },
-        ema200: {
-            type: Number,
-            validate: (ema) => ema >= 0,
-        },
-        macd: {
-            type: Number,
-        },
-        macdHist: {
-            type: Number,
-        },
-        macdSignal: {
-            type: Number,
-        },
-        rsi: {
-            type: Number,
-            validate: (rsi) => rsi >= 0.0 && rsi <= 100.0,
-        },
-        bbandLower: {
-            type: Number,
-        },
-        bbandUpper: {
-            type: Number,
-        },
-        bbandMiddle: {
-            type: Number,
-        },
-    }, {
+exports.TechnicalIndicatorsSchema = {
+    symbol: {
+        type: String,
+        validate: (symbol) => symbol.length > 0,
+        required: true,
+    },
+    date: {
+        type: String,
+        validate: (date) => date.length > 0,
+        required: true,
+        rangeKey: true,
+    },
+    sma15: {
+        type: Number,
+        validate: (sma) => sma >= 0,
+    },
+    sma20: {
+        type: Number,
+        validate: (sma) => sma >= 0,
+    },
+    sma50: {
+        type: Number,
+        validate: (sma) => sma >= 0,
+    },
+    sma100: {
+        type: Number,
+        validate: (sma) => sma >= 0,
+    },
+    sma200: {
+        type: Number,
+        validate: (sma) => sma >= 0,
+    },
+    ema12: {
+        type: Number,
+        validate: (ema) => ema >= 0,
+    },
+    ema20: {
+        type: Number,
+        validate: (ema) => ema >= 0,
+    },
+    ema26: {
+        type: Number,
+        validate: (ema) => ema >= 0,
+    },
+    ema50: {
+        type: Number,
+        validate: (ema) => ema >= 0,
+    },
+    ema100: {
+        type: Number,
+        validate: (ema) => ema >= 0,
+    },
+    ema200: {
+        type: Number,
+        validate: (ema) => ema >= 0,
+    },
+    macd: {
+        type: Number,
+    },
+    macdHist: {
+        type: Number,
+    },
+    macdSignal: {
+        type: Number,
+    },
+    rsi: {
+        type: Number,
+        validate: (rsi) => rsi >= 0.0 && rsi <= 100.0,
+    },
+    bbandLower: {
+        type: Number,
+    },
+    bbandUpper: {
+        type: Number,
+    },
+    bbandMiddle: {
+        type: Number,
+    },
+};
+exports.TechnicalIndicators = dynamoose.model(
+    'TechnicalIndicators',
+    new dynamoose.Schema(exports.TechnicalIndicatorsSchema, {
         timestamps: true,
     }),
     {
         create: CONFIG_CREATE,
         prefix: CONFIG_PREFIX,
         waitForActive: CONFIG_WAIT_FOR_ACTIVE,
-    });
+    },
+);
--- test/unit/db.js
+++ test/unit/db.js
@@ -7,35 +7,37 @@ const db = require('../../src/db');
 describe('db', () => {
     describe('#handleThroughput()', () => {
         it('should work for normal case', () => {
-            return db.handleThroughput(params => {
+            return db.handleThroughput((params) => {
                 expect(params).to.equal('123');
-            },
-            '123');
+            }, '123');
         });
 
         it('should work for ProvisionedThroughputExceededException', () => {
             class DynamoDBError extends Error {
-                constructor(code, ...params) {
+                constructor(type, ...params) {
                     super(...params);
-                    this.code = code;
+                    this.__type = type;
                 }
             }
 
             let thrown = false;
-            return db.handleThroughput(params => {
+            return db.handleThroughput((params) => {
                 if (!thrown) {
                     thrown = true;
-                    throw new DynamoDBError('ProvisionedThroughputExceededException');
+                    throw new DynamoDBError('#ProvisionedThroughputExceededException');
                 }
                 expect(params).to.equal('123');
-            },
-            '123');
+            }, '123');
         });
 
-        it('should work for other Exception', done => {
-            db.handleThroughput(params => { throw new Error('expected exception'); }, '123')
-                .then(() => { throw new Error("shouldn't be here"); })
-                .catch(err => {
+        it('should work for other Exception', (done) => {
+            db.handleThroughput((params) => {
+                throw new Error('expected exception');
+            }, '123')
+                .then(() => {
+                    throw new Error("shouldn't be here");
+                })
+                .catch((err) => {
                     if (err.message !== 'expected exception') {
                         // Evil hack: calling done() twice to make it fail, as re-throwing err here just results in a timeout :(
                         console.error(err);
@@ -46,70 +48,59 @@ describe('db', () => {
         });
     });
 
-    describe('#CompanyOverview.symbol.validate()', () => {
+    describe('#CompanyOverviewSchema.symbol.validate()', () => {
         it('should work for AMZN', () => {
-            const schemaObject = db.CompanyOverview.Model.schemas[0].schemaObject;
-            expect(schemaObject.symbol.validate('AMZN')).to.be.true;
+            expect(db.CompanyOverviewSchema.symbol.validate('AMZN')).to.be.true;
         });
     });
 
-    describe('#DailyAdjusted.symbol.validate()', () => {
+    describe('#DailyAdjustedSchema.symbol.validate()', () => {
         it('should work for AMZN', () => {
-            const schemaObject = db.DailyAdjusted.Model.schemas[0].schemaObject;
-            expect(schemaObject.symbol.validate('AMZN')).to.be.true;
+            expect(db.DailyAdjustedSchema.symbol.validate('AMZN')).to.be.true;
         });
     });
 
-    describe('#DailyAdjusted.date.validate()', () => {
+    describe('#DailyAdjustedSchema.date.validate()', () => {
         it('should work for 2021-01-04', () => {
-            const schemaObject = db.DailyAdjusted.Model.schemas[0].schemaObject;
-            expect(schemaObject.date.validate('2021-01-04')).to.be.true;
+            expect(db.DailyAdjustedSchema.date.validate('2021-01-04')).to.be.true;
         });
     });
 
-    describe('#TechnicalIndicators.symbol.validate()', () => {
+    describe('#TechnicalIndicatorsSchema.symbol.validate()', () => {
         it('should work for AMZN', () => {
-            const schemaObject = db.TechnicalIndicators.Model.schemas[0].schemaObject;
-            expect(schemaObject.symbol.validate('AMZN')).to.be.true;
+            expect(db.TechnicalIndicatorsSchema.symbol.validate('AMZN')).to.be.true;
         });
     });
 
-    describe('#TechnicalIndicators.date.validate()', () => {
+    describe('#TechnicalIndicatorsSchema.date.validate()', () => {
         it('should work for 2021-01-04', () => {
-            const schemaObject = db.TechnicalIndicators.Model.schemas[0].schemaObject;
-            expect(schemaObject.date.validate('2021-01-04')).to.be.true;
+            expect(db.TechnicalIndicatorsSchema.date.validate('2021-01-04')).to.be.true;
         });
     });
 
-    describe('#TechnicalIndicators.rsi.validate()', () => {
+    describe('#TechnicalIndicatorsSchema.rsi.validate()', () => {
         it('should fail for -0.1', () => {
-            const schemaObject = db.TechnicalIndicators.Model.schemas[0].schemaObject;
-            expect(schemaObject.rsi.validate(-0.1)).to.be.false;
+            expect(db.TechnicalIndicatorsSchema.rsi.validate(-0.1)).to.be.false;
         });
 
         it('should work for 0.0', () => {
-            const schemaObject = db.TechnicalIndicators.Model.schemas[0].schemaObject;
-            expect(schemaObject.rsi.validate(0.0)).to.be.true;
+            expect(db.TechnicalIndicatorsSchema.rsi.validate(0.0)).to.be.true;
         });
 
         it('should work for 30.0', () => {
-            const schemaObject = db.TechnicalIndicators.Model.schemas[0].schemaObject;
-            expect(schemaObject.rsi.validate(30.0)).to.be.true;
+            expect(db.TechnicalIndicatorsSchema.rsi.validate(30.0)).to.be.true;
         });
 
         it('should work for 70.0', () => {
-            const schemaObject = db.TechnicalIndicators.Model.schemas[0].schemaObject;
-            expect(schemaObject.rsi.validate(70.0)).to.be.true;
+            expect(db.TechnicalIndicatorsSchema.rsi.validate(70.0)).to.be.true;
         });
 
         it('should work for 100.0', () => {
-            const schemaObject = db.TechnicalIndicators.Model.schemas[0].schemaObject;
-            expect(schemaObject.rsi.validate(100.0)).to.be.true;
+            expect(db.TechnicalIndicatorsSchema.rsi.validate(100.0)).to.be.true;
         });
 
         it('should fail for 100.1', () => {
-            const schemaObject = db.TechnicalIndicators.Model.schemas[0].schemaObject;
-            expect(schemaObject.rsi.validate(100.1)).to.be.false;
+            expect(db.TechnicalIndicatorsSchema.rsi.validate(100.1)).to.be.false;
         });
     });
 });
